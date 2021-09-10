cabba02f9aa56b30e7162b5a1004573a313742d3
Javokhirbek2000 <j.mirzaakbarov.2000@gmail.com>
2021-09-11 00:14:57 +0500

    load cts fixed

--- controllers/loadControllers.js
+++ controllers/loadControllers.js
@@ -10,15 +10,18 @@ exports.getUsersLoads = catchAsync(async (req, res) => {
   let loads;
   if (req.user.role === 'DRIVER') {
     loads = await Load.find(
-      { assigned_to: req.user.id, status: status },
+      {
+        assigned_to: req.user.id,
+        status: status || /^(ASSIGNED|SHIPPED)$/,
+      },
       {},
-      { skip: +offset || 0, limit: +limit || 0 }
+      { skip: +offset || 0, limit: +limit || 10 }
     );
   } else if (req.user.role === 'SHIPPER') {
     loads = await Load.find(
       { created_by: req.user.id },
       {},
-      { skip: +offset || 0, limit: +limit || 0 }
+      { skip: +offset || 0, limit: +limit || 10 }
     );
   }
 
@@ -28,16 +31,20 @@ exports.getUsersLoads = catchAsync(async (req, res) => {
 });
 
 exports.addLoad = catchAsync(async (req, res) => {
+  if (req.user.role === 'DRIVER') {
+    return res.status(400).json({
+      message: 'This route is available only for shippers!',
+    });
+  }
   // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  const newLoad = await Load.create({ ...req.body, created_by: req.user.id });
+  await Load.create({ ...req.body, created_by: req.user.id });
   res.status(200).json({
     message: 'Load created successfully',
-    newLoad,
   });
 });
 
 exports.getActiveLoads = catchAsync(async (req, res) => {
-  if (!req.user.role === 'DRIVER') {
+  if (req.user.role === 'SHIPPER') {
     return res.status(400).json({
       message: 'Only drivers can access this route',
     });
@@ -54,15 +61,21 @@ exports.getActiveLoads = catchAsync(async (req, res) => {
 });
 
 exports.iterateToNextLoad = catchAsync(async (req, res) => {
+  if (req.user.role === 'SHIPPER') {
+    return res.status(400).json({
+      message: 'This route is available only for drivers!',
+    });
+  }
   const load = await Load.findOne({
     assigned_to: req.user.id,
     status: 'ASSIGNED',
   });
 
   const currentState = load.state || 'En route to pick up';
-  if (loadStates.indexOf(currentState === 2)) {
+  if (loadStates.indexOf(currentState) === 2) {
+    load.status = 'SHIPPED';
     return res.status(400).json({
-      message: 'Load is delivered',
+      message: 'Load is already delivered',
     });
   }
   load.state = loadStates[loadStates.indexOf(currentState) + 1];
@@ -70,6 +83,11 @@ exports.iterateToNextLoad = catchAsync(async (req, res) => {
   await load.save({
     validateBeforeSave: false,
   });
+  load.logs.push({
+    message: `Load state changed to ${load.state}`,
+    time: Date.now(),
+  });
+  await load.save({ validateBeforeSave: false });
 
   res.status(200).json({
     message: `Load state changed to ${load.state}`,
@@ -77,6 +95,11 @@ exports.iterateToNextLoad = catchAsync(async (req, res) => {
 });
 
 exports.getLoad = catchAsync(async (req, res) => {
+  if (req.user.role === 'DRIVER') {
+    return res.status(400).json({
+      message: 'This route is available only for shippers!',
+    });
+  }
   const load = await Load.findById(req.params.id);
   res.status(200).json({
     load,
@@ -84,24 +107,39 @@ exports.getLoad = catchAsync(async (req, res) => {
 });
 
 exports.updateLoad = catchAsync(async (req, res) => {
-  const newLoad = await Load.findByIdAndUpdate(req.params.id, req.body, {
+  if (req.user.role === 'DRIVER') {
+    return res.status(400).json({
+      message: 'This route is available only for shippers!',
+    });
+  }
+  await Load.findByIdAndUpdate(req.params.id, req.body, {
     runValidators: true,
     new: true,
   });
   res.status(200).json({
-    message: 'Load updated successfully!',
-    newLoad,
+    message: 'Load details changed successfully!',
   });
 });
 
 exports.deleteLoad = catchAsync(async (req, res) => {
-  await Load.findOneAndRemove(req.params.id);
+  if (req.user.role === 'DRIVER') {
+    return res.status(400).json({
+      message: 'This route is available only for shippers!',
+    });
+  }
+  await Load.findOneAndRemove({ _id: req.params.id });
   res.status(200).json({
     message: 'Load deleted successfully',
   });
 });
 
 exports.postLoad = catchAsync(async (req, res) => {
+  if (req.user.role === 'DRIVER') {
+    return res.status(400).json({
+      message: 'This route is available only for shippers!',
+    });
+  }
+
   let driverFound = false;
   const load = await Load.findById(req.params.id);
   const truckType = findTruckType({
@@ -147,7 +185,23 @@ exports.postLoad = catchAsync(async (req, res) => {
 });
 
 exports.getShippingInfo = catchAsync(async (req, res) => {
+  if (req.user.role === 'DRIVER') {
+    return res.status(400).json({
+      message: 'This route is available only for shippers!',
+    });
+  }
+
+  const load = await Load.findOne({
+    _id: req.params.id,
+    status: { $ne: 'NEW' },
+  });
+
+  if (!load) {
+    return res.status(400).json({
+      message: 'No load found',
+    });
+  }
   res.status(200).json({
-    message: 'This controller is not ready yet!',
+    load,
   });
 });
